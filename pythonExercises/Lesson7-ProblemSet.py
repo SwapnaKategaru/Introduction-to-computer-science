2e9842451838ae44950fcd8dddc880c1a50759bb
rajas <swapna123@gmail.com>
2019-07-05 21:05:17 +0530

    Completed quizzes of lesson 7

---
+++ pythonExercises/Lesson7-ProblemSet.py
@@ -0,0 +1,18 @@
+# """   WEEKEND QUIZ -1   """
+
+# Define a procedure weekend which takes a string as its input, and
+# returns the boolean True if it's 'Saturday' or 'Sunday' and False otherwise.
+
+
+def weekend(day):
+    if day == "Saturday" or day == "Sunday":
+        return True
+    return False
+
+
+print(weekend("Monday"))
+print(weekend('Saturday'))
+print(weekend('July'))
+# --------------------------> RESULT False True False
+
+# ######################################################################################################################
